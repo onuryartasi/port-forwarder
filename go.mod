6064f2e17a59d011dcd6b428d28b596cc80260de
Onur Yartaşı <onuryartasi@live.com>
2021-05-21 19:01:07 +0300

    added config and dummy code

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/onuryartasi/kubernetes-proxy
+
+go 1.16
+
+require (
+	github.com/mitchellh/mapstructure v1.4.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0
+)
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+)
+
+type ProxyConfig struct {
+	Namespace string  `yaml:"namespace,omitempty"`
+	Service map[string][]struct{
+		Port       string `yaml:"port"`
+		TargetPort string `yaml:"targetPort"`
+		namespace string `yaml:"namespace",omitempty`
+	} `yaml:"service,omitempty"`
+}
+
+func main() {
+
+	_, err := exec.Command("which", "kubectl").CombinedOutput()
+	if err != nil {
+		fmt.Println("Please install kubectl tool")
+	}
+	proxyConfig := ProxyConfig{}
+	namespace:="stage"
+	config, err := ioutil.ReadFile("proxy-config.yaml")
+	if err != nil {
+		log.Printf("yamlFile.Get err  #%v ", err)
+	}
+	err = yaml.Unmarshal(config, &proxyConfig)
+	pids := []*os.Process{}
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	for serviceName, service := range proxyConfig.Service {
+		for _, elm := range service {
+			cmd := exec.Command("/bin/bash", "-c", fmt.Sprintf(" kubectl port-forward -n %s svc/%s %s:%s",
+				namespace,serviceName, service[0].Port, service[0].TargetPort))
+			err = cmd.Start()
+			if err != nil {
+				log.Printf("Cannot proxied %s", serviceName)
+			} else {
+				log.Printf("%s proxied %s-->%s", serviceName, elm.Port, elm.TargetPort)
+				pids = append(pids, cmd.Process)
+			}
+
+		}
+
+	}
+
+	for {
+
+
+	}
+}
