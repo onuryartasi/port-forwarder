de6d822df78a6c508a058050a03a1533e18daf2c
Onur Yartaşı <onuryartasi@live.com>
2021-05-25 21:16:43 +0300

    fix catch error

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
-	"os"
 	"os/exec"
 )
 
@@ -14,13 +13,18 @@ type ProxyConfig struct {
 	Service map[string][]struct{
 		Port       string `yaml:"port"`
 		TargetPort string `yaml:"targetPort"`
-		namespace string `yaml:"namespace",omitempty`
+		Namespace string `yaml:"namespace,omitempty"`
 	} `yaml:"service,omitempty"`
 }
 
-func main() {
 
-	_, err := exec.Command("which", "kubectl").CombinedOutput()
+
+type Response struct {
+	ServiceName string
+	Message string
+}
+func main() {
+	_, err := exec.LookPath("kubectl")
 	if err != nil {
 		log.Fatalf("Please install kubectl tool")
 	}
@@ -35,31 +39,38 @@ func main() {
 	if len(proxyConfig.Namespace) != 0 {
 		namespace = proxyConfig.Namespace
 	}
-	pids := []*os.Process{}
 	if err != nil {
 		log.Fatalf("error: %v", err)
 	}
+	outputChan := make(chan Response)
 
+
+	fmt.Println("Global namespace: ",namespace)
+	ns := namespace
 	for serviceName, service := range proxyConfig.Service {
 		for _, elm := range service {
-			fmt.Printf(namespace)
-			cmd := exec.Command("/bin/bash", "-c", fmt.Sprintf(" kubectl port-forward --namespace %s svc/%s %s:%s",
-				namespace,serviceName, service[0].Port, service[0].TargetPort))
-			err := cmd.Start()
-
-			if err != nil {
-				log.Printf("Cannot proxied %s", serviceName)
-			} else {
-				log.Printf("%s proxied %s-->%s", serviceName, elm.Port, elm.TargetPort)
-				pids = append(pids, cmd.Process)
+			if len(elm.Namespace) != 0 {
+				ns = elm.Namespace
 			}
+			log.Printf("%s service on namespace %s proxied %s --> %s",serviceName,ns,elm.Port,elm.TargetPort)
+			go func(serviceName,port,targetPort,ns string){
+
+				cmd := exec.Command("/bin/bash", "-c", fmt.Sprintf(" kubectl port-forward --namespace %s svc/%s %s:%s",
+					ns,serviceName, port, targetPort))
+
+				output,err := cmd.CombinedOutput()
+				if err != nil {
+					outputChan <- Response{ServiceName: serviceName,Message: string(output)}
+				}
+			}(serviceName,elm.Port,elm.TargetPort,ns)
 
 		}
-
 	}
 
-	for {
 
+
+	for msg := range outputChan{
+		log.Println(msg)
 
 	}
 }
